3634617b3c838c31951f9352f5f5427181111767
Highjhacker <jolanbeer@gmail.com>
2020-11-19 17:11:33 +0100

    fix: wrong localization keys

--- resources/views/profile/delete-user-form.blade.php
+++ resources/views/profile/delete-user-form.blade.php
@@ -1,8 +1,8 @@
 <div>
-    <div class="flex flex-col">
-        <span class="text-2xl font-semibold text-theme-secondary-900">@lang('fortify::pages.delete_account_title')</span>
+    <div class="flex flex-col px-8 lg:p-8 xl:mx-8">
+        <span class="text-2xl font-semibold text-theme-secondary-900">@lang('fortify::pages.user-settings.delete_account_title')</span>
         <span>
-            @lang('fortify::pages.delete_account_description')
+            @lang('fortify::pages.user-settings.delete_account_description')
         </span>
 
         <div class="flex flex-row justify-end mt-8">
@@ -34,10 +34,10 @@
             <x-slot name="buttons">
                 <div class="flex flex-col w-full sm:flex-row justify-end mt-5 space-y-4 sm:space-y-0 sm:space-x-3">
                     <button class="button-secondary"
-                        wire:click="$toggle('confirmingUserDeletion')">@lang('actions.cancel')</button>
+                        wire:click="$toggle('confirmingUserDeletion')">@lang('fortify::actions.cancel')</button>
                     <button class="inline-flex justify-center items-center button-primary" wire:click="destroy">
                         @svg('trash', 'h-4 w-4')
-                        <span class="ml-2">@lang('actions.delete')</span>
+                        <span class="ml-2">@lang('fortify::actions.delete')</span>
                     </button>
                 </div>
             </x-slot>

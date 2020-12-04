d53584e4be0a7448500a97fcbc046940e208eafd
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2020-12-04 13:44:52 +0000

    refactor: update headings

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -14,7 +14,9 @@
 @section('content')
     <div class="container mx-auto">
         <div class="mx-auto my-8 md:w-3/4 lg:w-3/5 xl:w-1/2">
-            <h1 class="mx-4 text-2xl font-bold md:text-4xl md:mx-8 xl:mx-16">@lang('fortify::auth.forgot-password.page_header')</h1>
+            <h1 class="mx-4 text-2xl font-bold md:text-4xl md:mx-8 xl:mx-16 text-center">
+                @lang('fortify::auth.forgot-password.page_header')
+            </h1>
 
             <div class="px-8">
                 <x-ark-flash />

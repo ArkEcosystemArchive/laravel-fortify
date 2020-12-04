62415ad6494d6fb12a33598765cb8eb004e0e08f
alfonsobries <alfonsobries@users.noreply.github.com>
2020-12-04 18:02:52 +0000

    style: resolve style guide violations

--- tests/Http/Responses/SuccessfulPasswordResetLinkRequestResponseTest.php
+++ tests/Http/Responses/SuccessfulPasswordResetLinkRequestResponseTest.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
+use ARKEcosystem\Fortify\Http\Responses\SuccessfulPasswordResetLinkRequestResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
-use ARKEcosystem\Fortify\Http\Responses\SuccessfulPasswordResetLinkRequestResponse;
 
 it('The success response flash a message', function () {
     $response = new SuccessfulPasswordResetLinkRequestResponse(Password::RESET_LINK_SENT);
     $response = $response->toResponse(new Request());
 
     expect($response->getStatusCode())->toBe(302);
-    
+
     expect(app('session.store')->get('laravel_flash_message'))->toHaveKey('message');
 });

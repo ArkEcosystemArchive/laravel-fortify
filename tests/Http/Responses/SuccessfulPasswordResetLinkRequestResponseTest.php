54f7630aa3979067acae12abde121f5cffa5f3ff
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-12-04 12:01:20 -0600

    Add test coverage for the new responses

---
+++ tests/Http/Responses/SuccessfulPasswordResetLinkRequestResponseTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use ARKEcosystem\Fortify\Http\Responses\SuccessfulPasswordResetLinkRequestResponse;
+
+it('The success response flash a message', function () {
+    $response = new SuccessfulPasswordResetLinkRequestResponse(Password::RESET_LINK_SENT);
+    $response = $response->toResponse(new Request());
+
+    expect($response->getStatusCode())->toBe(302);
+    
+    expect(app('session.store')->get('laravel_flash_message'))->toHaveKey('message');
+});

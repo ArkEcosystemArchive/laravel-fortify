f8bb499a13057d8aca77174656ff26cd12106b68
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2020-12-11 11:28:08 +0100

    chore: update forms padding (#32)

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -1,27 +1,26 @@
 <div x-data="{isTyping: false}">
-    <div class="flex flex-col">
-        <div class="my-8">
+    <div class="flex flex-col space-y-4">
+        <div>
             <x-ark-alert type="warning" :message="trans('fortify::pages.user-settings.update_password_alert_description')" />
         </div>
         <div>
             <x-ark-flash />
         </div>
-        <span class="mt-4 header-4">@lang('fortify::pages.user-settings.password_information_title')</span>
-        <span class="mt-4">@lang('fortify::forms.update-password.requirements_notice')</span>
-
-        <form class="mt-8" wire:submit.prevent="updatePassword">
-            <div class="space-y-4">
-                <x-ark-input type="password" name="current_password" model="state.current_password" :label="trans('fortify::forms.current_password')" :errors="$errors" />
-
-                <x:ark-fortify::password-rules class="w-full" :password-rules="$passwordRules" is-typing="isTyping">
-                    <x-ark-input type="password" name="password" model="state.password" class="w-full" :label="trans('fortify::forms.new_password')" @keydown="isTyping=true" :errors="$errors" />
-                </x:ark-fortify::password-rules>
-
-                <x-ark-input type="password" name="password_confirmation" model="state.password_confirmation" :label="trans('fortify::forms.confirm_password')" :errors="$errors" />
-            </div>
-            <div class="flex sm:justify-end mt-8 w-full">
-                <button type="submit" class="button-secondary w-full sm:w-auto">@lang('fortify::actions.update')</button>
-            </div>
-        </form>
+        <span class="header-4">@lang('fortify::pages.user-settings.password_information_title')</span>
+        <span>@lang('fortify::forms.update-password.requirements_notice')</span>
     </div>
+    <form class="mt-8" wire:submit.prevent="updatePassword">
+        <div class="space-y-4">
+            <x-ark-input type="password" name="current_password" model="state.current_password" :label="trans('fortify::forms.current_password')" :errors="$errors" />
+
+            <x:ark-fortify::password-rules class="w-full" :password-rules="$passwordRules" is-typing="isTyping">
+                <x-ark-input type="password" name="password" model="state.password" class="w-full" :label="trans('fortify::forms.new_password')" @keydown="isTyping=true" :errors="$errors" />
+            </x:ark-fortify::password-rules>
+
+            <x-ark-input type="password" name="password_confirmation" model="state.password_confirmation" :label="trans('fortify::forms.confirm_password')" :errors="$errors" />
+        </div>
+        <div class="flex sm:justify-end mt-8 w-full">
+            <button type="submit" class="button-secondary w-full sm:w-auto">@lang('fortify::actions.update')</button>
+        </div>
+    </form>
 </div>
--- resources/views/profile/update-timezone-form.blade.php
+++ resources/views/profile/update-timezone-form.blade.php
@@ -1,24 +1,23 @@
 <div>
-    <div class="flex flex-col">
-        <span class="text-2xl font-semibold text-theme-secondary-900">@lang('fortify::pages.user-settings.update_timezone_title')</span>
+    <div class="flex flex-col space-y-4">
+        <span class="header-4">@lang('fortify::pages.user-settings.update_timezone_title')</span>
         <span>@lang('fortify::pages.user-settings.update_timezone_description')</span>
         <div>
             <x-ark-flash />
         </div>
-
-        <div class="relative mt-8 space-y-4">
-            <x-ark-select :label="trans('fortify::actions.select_timezone')" :errors="$errors" name="timezone">
-                @foreach ($this->timezones as $timezone)
-                    @if ($timezone['timezone'] === $this->currentTimezone)
-                        <option value="{{ $timezone['timezone'] }}" selected>{{ $timezone['formattedTimezone'] }}</option>
-                    @else
-                        <option value="{{ $timezone['timezone'] }}">{{ $timezone['formattedTimezone'] }}</option>
-                    @endif
-                @endforeach
-            </x-ark-select>
-        </div>
-        <div class="flex justify-end mt-8">
-            <button type="submit" class="button-secondary" wire:click="updateTimezone">@lang('fortify::actions.update')</button>
-        </div>
+    </div>
+    <div class="relative mt-8 space-y-4">
+        <x-ark-select :label="trans('fortify::actions.select_timezone')" :errors="$errors" name="timezone">
+            @foreach ($this->timezones as $timezone)
+                @if ($timezone['timezone'] === $this->currentTimezone)
+                    <option value="{{ $timezone['timezone'] }}" selected>{{ $timezone['formattedTimezone'] }}</option>
+                @else
+                    <option value="{{ $timezone['timezone'] }}">{{ $timezone['formattedTimezone'] }}</option>
+                @endif
+            @endforeach
+        </x-ark-select>
+    </div>
+    <div class="flex justify-end mt-8">
+        <button type="submit" class="button-secondary" wire:click="updateTimezone">@lang('fortify::actions.update')</button>
     </div>
 </div>

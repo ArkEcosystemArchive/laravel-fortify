84801a9badb4e2193a2f41602b4783e789e9b67a
Highjhacker <jolanbeer@gmail.com>
2021-01-04 13:51:06 +0100

    refactor: allow to specify custom dimensions and alignment for update profile photo component

--- src/Components/UpdateProfilePhotoForm.php
+++ src/Components/UpdateProfilePhotoForm.php
@@ -13,7 +13,9 @@ class UpdateProfilePhotoForm extends Component
     use InteractsWithUser;
     use WithFileUploads;
 
+    public $alignment;
     public $photo;
+    public $dimensions;
 
     /**
      * Render the component.
--- tests/Components/UpdateProfilePhotoFormTest.php
+++ tests/Components/UpdateProfilePhotoFormTest.php
@@ -8,7 +8,7 @@ use Livewire\Livewire;
 use Spatie\MediaLibrary\MediaCollections\FileAdderFactory;
 use Tests\MediaUser;
 
-it('can_upload_a_photo', function () {
+it('can upload a photo', function () {
     $this
         ->mock(FileAdderFactory::class)
         ->shouldReceive('createFromDisk->toMediaCollection')
@@ -21,7 +21,7 @@ it('can_upload_a_photo', function () {
         ->set('photo', $photo);
 });
 
-it('cannot_upload_a_photo_with_disallowed_extension', function () {
+it('cannot upload a photo with disallowed extension', function () {
     $photo = UploadedFile::fake()->create('logo.gif', 1000, 'image/gif');
 
     Livewire::actingAs(MediaUser::fake())
@@ -30,11 +30,33 @@ it('cannot_upload_a_photo_with_disallowed_extension', function () {
         ->assertHasErrors('photo');
 });
 
-it('cannot_upload_a_photo_that_is_too_large', function () {
+it('cannot upload a photo that is too large', function () {
     $photo = UploadedFile::fake()->image('logo.jpeg')->size(5000);
 
     Livewire::actingAs(MediaUser::fake())
         ->test(UpdateProfilePhotoForm::class)
         ->set('photo', $photo)
         ->assertHasErrors('photo');
+});
+
+it('can have custom dimensions', function () {
+    Livewire::actingAs(MediaUser::fake())
+        ->test(UpdateProfilePhotoForm::class)
+        ->assertSet('dimensions', null)
+        ->assertSee('w-48 h-48')
+        ->assertDontSee('w-24 h-24')
+        ->set('dimensions', 'w-24 h-24')
+        ->assertSet('dimensions', 'w-24 h-24')
+        ->assertSee('w-24 h-24');
+});
+
+it('can have a custom alignment', function () {
+    Livewire::actingAs(MediaUser::fake())
+        ->test(UpdateProfilePhotoForm::class)
+        ->assertSet('alignment', null)
+        ->assertSee('items-center mb-4 md:items-start')
+        ->set('alignment', 'items-end')
+        ->assertSet('alignment', 'items-end')
+        ->assertSee('items-end')
+        ->assertDontSee('items-center mb-4 md:items-start');
 });

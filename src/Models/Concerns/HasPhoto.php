8f9776822033dcd9395b3bfa52a270afbe2bd19d
Brian Faust <faustbrian@users.noreply.github.com>
2021-01-13 17:42:20 +0200

    fix: add `InteractsWithMedia` to `HasPhoto`

--- src/Models/Concerns/HasPhoto.php
+++ src/Models/Concerns/HasPhoto.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace ARKEcosystem\Fortify\Models\Concerns;
 
+use Spatie\MediaLibrary\InteractsWithMedia;
+
 trait HasPhoto
 {
+    use InteractsWithMedia;
+
     public function getPhotoAttribute(): string
     {
         return $this->getFirstMediaUrl('photo');

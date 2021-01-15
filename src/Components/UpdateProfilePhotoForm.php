1fc3d7890ab08e76728a0f53e57989707487f498
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-15 14:50:03 +0000

    refactor: extend ui dep upload image class

--- src/Components/UpdateProfilePhotoForm.php
+++ src/Components/UpdateProfilePhotoForm.php
@@ -5,19 +5,11 @@ declare(strict_types=1);
 namespace ARKEcosystem\Fortify\Components;
 
 use ARKEcosystem\Fortify\Components\Concerns\InteractsWithUser;
-use Livewire\Component;
-use Livewire\WithFileUploads;
+use ARKEcosystem\UserInterface\Components\UploadImage;
 
-class UpdateProfilePhotoForm extends Component
+class UpdateProfilePhotoForm extends UploadImage
 {
     use InteractsWithUser;
-    use WithFileUploads;
-
-    public $alignment;
-
-    public $photo;
-
-    public $dimensions;
 
     /**
      * Render the component.
@@ -29,15 +21,10 @@ class UpdateProfilePhotoForm extends Component
         return view('ark-fortify::profile.update-profile-photo-form');
     }
 
-    public function updatedPhoto()
-    {
-        $this->store();
-    }
-
     public function store()
     {
         $this->validate([
-            'photo' => ['mimes:jpeg,png,bmp,jpg', 'max:2048'],
+            'photo' => $this->validators(),
         ]);
 
         $file = $this->photo->storePubliclyAs('uploads', $this->photo->hashName());

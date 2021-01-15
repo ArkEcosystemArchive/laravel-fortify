fa4bb6662ad40d99143f9727a6a8d23c200fe305
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-15 00:14:31 +0000

    refactor: use component from ui

--- resources/views/profile/update-profile-photo-form.blade.php
+++ resources/views/profile/update-profile-photo-form.blade.php
@@ -6,31 +6,12 @@
     class="relative flex flex-col {{ $alignment ?? 'items-center mb-4 md:items-start' }}"
 >
     <form wire:submit.prevent="store" id="livewire-form">
-        <div
-            style="background-image: url('{{ $this->user->photo }}')"
-            class="cursor-pointer bg-theme-secondary-200 inline-block bg-contain bg-center bg-no-repeat rounded {{ $dimensions ?? 'w-48 h-48' }}"
-            role="button"
-        >
-            <input
-                id="photo"
-                type="file"
-                class="absolute top-0 hidden block opacity-0 cursor-pointer"
-                wire:model="photo"
-                accept="image/jpg,image/jpeg,image/bmp,image/png"
-            />
-        </div>
-
-        <div x-show="isUploading" x-cloak>
-            <x-ark-loading-spinner />
-        </div>
-
-        <div
-            class="cursor-pointer flex items-center justify-center rounded absolute top-0 opacity-0 hover:opacity-90 transition-default bg-theme-secondary-900 {{ $dimensions ?? 'w-48 h-48' }}"
-            @click="select()"
-            role="button"
-        >
-            <div>@svg('upload', 'w-4 h-4 text-white')</div>
-        </div>
+        {{-- TODO: Add max file size & min image size --}}
+        <x-ark-upload-image
+            :image="$this->user->photo"
+            :upload-text="__('fortify::forms.upload-avatar.upload_avatar')"
+            :delete-tooltip="__('fortify::forms.upload-avatar.delete_avatar')"
+        />
     </form>
 
     @error('photo')

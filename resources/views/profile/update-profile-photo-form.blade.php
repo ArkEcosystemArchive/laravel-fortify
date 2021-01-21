35aa2472570d036445b85d17fbb2d7dc56040a66
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-21 15:51:09 +0000

    refactor: re-add removed readonly property

--- resources/views/profile/update-profile-photo-form.blade.php
+++ resources/views/profile/update-profile-photo-form.blade.php
@@ -2,6 +2,7 @@
     <form wire:submit.prevent="store" id="livewire-form">
         <x-ark-upload-image-single
             id="profile-image"
+            :readonly="$readonly"
             :image="$this->user->photo"
             :upload-text="__('fortify::forms.upload-avatar.upload_avatar')"
             :delete-tooltip="__('fortify::forms.upload-avatar.delete_avatar')"

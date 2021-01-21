a7100eb5fc38fadf3bf43afce70250de4e64429b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-21 14:00:39 +0000

    refactor: cater for single image upload changes

--- resources/views/profile/update-profile-photo-form.blade.php
+++ resources/views/profile/update-profile-photo-form.blade.php
@@ -1,21 +1,14 @@
-<div
-    x-data="{ isUploading: false, select() { document.getElementById('photo').click(); } }"
-    x-on:livewire-upload-start="isUploading = true"
-    x-on:livewire-upload-finish="isUploading = false"
-    x-on:livewire-upload-error="isUploading = false"
-    class="relative flex flex-col {{ $alignment ?? 'items-center mb-4 md:items-start' }}"
->
+<div class="relative flex flex-col {{ $alignment ?? 'items-center mb-4 md:items-start' }}">
     <form wire:submit.prevent="store" id="livewire-form">
-        <x-ark-upload-image
-            :readonly="$readonly"
+        <x-ark-upload-image-single
+            id="profile-image"
             :image="$this->user->photo"
             :upload-text="__('fortify::forms.upload-avatar.upload_avatar')"
             :delete-tooltip="__('fortify::forms.upload-avatar.delete_avatar')"
-            :dimensions="$dimensions"
         />
     </form>
 
-    @error('photo')
+    @error('singleImage')
         <p class="input-help--error">{{ $message }}</p>
     @enderror
 </div>
--- src/Components/UpdateProfilePhotoForm.php
+++ src/Components/UpdateProfilePhotoForm.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace ARKEcosystem\Fortify\Components;
 
 use ARKEcosystem\Fortify\Components\Concerns\InteractsWithUser;
-use ARKEcosystem\UserInterface\Components\UploadImage;
+use ARKEcosystem\UserInterface\Components\UploadImageSingle;
+use Livewire\Component;
 
-class UpdateProfilePhotoForm extends UploadImage
+class UpdateProfilePhotoForm extends Component
 {
     use InteractsWithUser;
+    use UploadImageSingle;
 
     /**
      * Render the component.
@@ -21,19 +23,19 @@ class UpdateProfilePhotoForm extends UploadImage
         return view('ark-fortify::profile.update-profile-photo-form');
     }
 
-    public function store()
+    public function updatedImageSingle()
     {
         $this->validate([
-            'photo' => $this->validators(),
+            'imageSingle' => $this->imageSingleValidators,
         ]);
 
-        $file = $this->photo->storePubliclyAs('uploads', $this->photo->hashName());
+        $file = $this->imageSingle->storePubliclyAs('uploads', $this->imageSingle->hashName());
 
         $this->user->addMediaFromDisk($file)->toMediaCollection('photo');
         $this->user->refresh();
     }
 
-    public function delete()
+    public function deleteImageSingle()
     {
         $this->user->getFirstMedia('photo')->delete();
         $this->user->refresh();

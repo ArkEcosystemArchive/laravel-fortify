142e3b96a72638abe856b20fdcbff766251e7c64
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-01-21 16:32:02 +0000

    refactor: cater for single image upload changes (#42)

--- resources/views/profile/update-profile-photo-form.blade.php
+++ resources/views/profile/update-profile-photo-form.blade.php
@@ -1,21 +1,15 @@
-<div
-    x-data="{ isUploading: false, select() { document.getElementById('photo').click(); } }"
-    x-on:livewire-upload-start="isUploading = true"
-    x-on:livewire-upload-finish="isUploading = false"
-    x-on:livewire-upload-error="isUploading = false"
-    class="relative flex flex-col {{ $alignment ?? 'items-center mb-4 md:items-start' }}"
->
+<div class="relative flex flex-col {{ $alignment ?? 'items-center mb-4 md:items-start' }}">
     <form wire:submit.prevent="store" id="livewire-form">
-        <x-ark-upload-image
+        <x-ark-upload-image-single
+            id="profile-image"
             :readonly="$readonly"
             :image="$this->user->photo"
             :upload-text="__('fortify::forms.upload-avatar.upload_avatar')"
             :delete-tooltip="__('fortify::forms.upload-avatar.delete_avatar')"
-            :dimensions="$dimensions"
         />
     </form>
 
-    @error('photo')
+    @error('singleImage')
         <p class="input-help--error">{{ $message }}</p>
     @enderror
 </div>

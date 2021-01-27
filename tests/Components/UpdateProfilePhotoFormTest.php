f174f9db4c241f51773fbc30fac1e1e67cf0ad6c
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-01-27 09:26:37 -0300

    Tests for UpdateProfilePhotoForm

--- tests/Components/UpdateProfilePhotoFormTest.php
+++ tests/Components/UpdateProfilePhotoFormTest.php
@@ -6,6 +6,9 @@ use ARKEcosystem\Fortify\Components\UpdateProfilePhotoForm;
 use Illuminate\Http\UploadedFile;
 use Livewire\Livewire;
 use Spatie\MediaLibrary\MediaCollections\FileAdderFactory;
+use Spatie\MediaLibrary\MediaCollections\MediaRepository;
+use Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Tests\MediaUser;
 
 it('can upload a photo', function () {
@@ -37,4 +40,36 @@ it('cannot upload a photo that is too large', function () {
         ->test(UpdateProfilePhotoForm::class)
         ->set('imageSingle', $photo)
         ->assertHasErrors('imageSingle');
+});
+
+it('can delete a photo', function () {
+    $this
+        ->mock(FileAdderFactory::class)
+        ->shouldReceive('createFromDisk->toMediaCollection')
+        ->once();
+
+    $media = Mockery::mock(Media::class);
+    $media->shouldReceive('delete');
+
+    $collection = Mockery::mock(MediaCollection::class);
+    $collection
+        ->shouldReceive('collectionName')
+        ->andReturnSelf();
+    $collection
+        ->shouldReceive('first')
+        ->andReturn($media);
+
+    $this->mock(MediaRepository::class)
+        ->shouldReceive('getCollection')
+        ->andReturn($collection);
+
+    $photo = UploadedFile::fake()
+        ->image('logo.jpeg')
+        ->size(1);
+
+    Livewire::actingAs(MediaUser::fake())
+        ->test(UpdateProfilePhotoForm::class)
+        ->set('imageSingle', $photo)
+        ->call('deleteImageSingle')
+        ->assertHasNoErrors('imageSingle');
 });

31ba341c6adb00c0384d035c1630265902011008
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-01-27 14:37:12 -0300

    Tests for TwoFactorAuthenticationForm

---
+++ tests/Components/TwoFactorAuthenticationFormTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use ARKEcosystem\Fortify\Components\TwoFactorAuthenticationForm;
+use Livewire\Livewire;
+use PragmaRX\Google2FALaravel\Google2FA;
+use function Tests\createUserModel;
+
+it('can interact with the form', function () {
+    $user = createUserModel();
+
+    $g2FA = $this->mock(Google2FA::class);
+    $g2FA->shouldReceive('verifyKey')
+        ->andReturnTrue();
+    app()->instance('pragmarx.google2fa', $g2FA);
+
+    $two_factor_secret = 'QHBRXHLWOT3B2T3L';
+    Livewire::actingAs($user)
+        ->test(TwoFactorAuthenticationForm::class)
+        ->assertSee('You have not enabled two factor authentication.')
+        ->set('state.two_factor_secret', $two_factor_secret)
+        ->assertSet('enabled', false)
+        ->assertSee($two_factor_secret)
+        ->set('state.otp', '843733')
+        ->call('enableTwoFactorAuthentication')
+        ->assertSee('Two-Factor Authentication Reset Code')
+        ->assertSee('If you lose your two-factor authentication device')
+        ->call('regenerateRecoveryCodes')
+        ->assertSee('Two-Factor Authentication Reset Code')
+        ->call('hideRecoveryCodes')
+        ->assertSee('You have enabled two factor authentication')
+        ->call('disableTwoFactorAuthentication')
+        ->assertSee('You have not enabled two factor authentication');
+});

290749efc242960e836bd28c7bfe0d1d7791c44c
Brian Faust <hello@basecode.sh>
2021-02-04 12:08:46 +0200

    wip

--- config/profanities.php
+++ config/profanities.php
@@ -3,10 +3,10 @@
 declare(strict_types=1);
 
 return [
-    'en' => [
-        'cunt',
-        'shit',
-        'fag',
-        'penis',
-    ],
+
+    'cunt',
+    'shit',
+    'fag',
+    'penis',
+
 ];

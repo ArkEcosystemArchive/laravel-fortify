ae4f39b32aabaad811987e2647a703de8a793ea3
Maurizio <maurizio.lepora@gmail.com>
2021-02-10 17:27:41 +0100

    refactor: update button size on 375px breakpoint

--- resources/views/profile/export-user-data.blade.php
+++ resources/views/profile/export-user-data.blade.php
@@ -3,7 +3,7 @@
         <span class="header-4">@lang('fortify::pages.user-settings.gdpr_title')</span>
         <span class="mt-4">@lang('fortify::pages.user-settings.gdpr_description')</span>
 
-        <div class="flex justify-end mt-8">
+        <div class="w-full sm:w-auto flex justify-end mt-8">
             <button type="submit" class="button-secondary" wire:click="export">@lang('fortify::actions.export_personal_data')</button>
         </div>
     </div>

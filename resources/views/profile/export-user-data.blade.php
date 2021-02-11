51fa08ee67a45db1cff3b192f0744405cc7fb56b
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-02-11 11:44:37 +0100

    chore: update export data button on mobile

--- resources/views/profile/export-user-data.blade.php
+++ resources/views/profile/export-user-data.blade.php
@@ -3,8 +3,8 @@
         <span class="header-4">@lang('fortify::pages.user-settings.gdpr_title')</span>
         <span class="mt-4">@lang('fortify::pages.user-settings.gdpr_description')</span>
 
-        <div class="w-full sm:w-auto flex justify-end mt-8">
-            <button type="submit" class="button-secondary" wire:click="export">@lang('fortify::actions.export_personal_data')</button>
+        <div class="flex justify-end mt-8">
+            <button type="submit" class="button-secondary w-full sm:auto" wire:click="export">@lang('fortify::actions.export_personal_data')</button>
         </div>
     </div>
 </div>

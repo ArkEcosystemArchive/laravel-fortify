a79f37ad9a47acee4db2a9501e8ddc930514fb2d
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-02-11 12:02:45 +0100

    fix: button tweaks (#62)

--- resources/views/profile/export-user-data.blade.php
+++ resources/views/profile/export-user-data.blade.php
@@ -4,7 +4,7 @@
         <span class="mt-4">@lang('fortify::pages.user-settings.gdpr_description')</span>
 
         <div class="flex justify-end mt-8">
-            <button type="submit" class="button-secondary w-full sm:auto" wire:click="export">@lang('fortify::actions.export_personal_data')</button>
+            <button type="submit" class="button-secondary w-full sm:w-auto" wire:click="export">@lang('fortify::actions.export_personal_data')</button>
         </div>
     </div>
 </div>

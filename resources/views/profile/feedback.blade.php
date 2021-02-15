95ea0c0bb59c6fc8f4ac3f0af14d06e8d4dfa224
Maurizio <maurizio.lepora@gmail.com>
2021-02-15 14:30:31 +0100

    refactor: use <x-ark-container> instead of <x-generic.container>

--- resources/views/profile/feedback.blade.php
+++ resources/views/profile/feedback.blade.php
@@ -6,13 +6,13 @@
 
 @section('content')
 
-<x-generic.container>
+<x-ark-container>
     <div class="mx-auto flex flex-col items-center w-full text-center">
         <h1 class="max-w-sm md:max-w-none">@lang('fortify::pages.feedback.title')</h1>
         <p class="px-5 mt-4 sm:px-12 md:px-0 md:max-w-2xl">@lang('fortify::pages.feedback.description')</p>
 
         <livewire:profile.send-feedback-form />
     </div>
-</x-generic.container>
+</x-ark-container>
 
 @endsection

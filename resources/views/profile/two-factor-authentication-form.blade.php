3649460b7e22c1dd136cec1aa5fe98d44da7e3a1
Highjhacker <jolanbeer@gmail.com>
2021-02-16 17:11:35 +0100

    wip

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -110,7 +110,7 @@
         </div>
     @endif
 
-    @if(!$this->modalShown)
+    @if($this->modalShown)
         <x-ark-modal title-class="header-2">
             @slot('title')
                 @lang('fortify::pages.user-settings.2fa_reset_code_title')

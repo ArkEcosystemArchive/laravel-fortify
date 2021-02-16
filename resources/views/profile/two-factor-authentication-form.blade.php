d19a97f09162917ff86a3c88d5609470d658d3f5
Highjhacker <jolanbeer@gmail.com>
2021-02-16 10:09:24 +0100

    wip

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -1,46 +1,89 @@
 <div>
     @if (! $this->enabled)
-        <div class="flex flex-col">
-            <span class="header-4">@lang('fortify::pages.user-settings.2fa_title')</span>
-            <span class="mt-4">@lang('fortify::pages.user-settings.2fa_description')</span>
+        <div class="flex flex-col w-full space-y-8">
+            <div class="flex flex-col">
+                <span class="text-lg font-bold leading-7 text-theme-secondary-900 md:header-4">
+                    @lang('fortify::pages.user-settings.2fa_title')
+                </span>
+                <span class="mt-4">
+                    @lang('fortify::pages.user-settings.2fa_description')
+                </span>
+            </div>
 
-            <div class="flex justify-start mt-8">
-                <div class="flex md:divide-x md:divide-theme-secondary-400">
-                    <div class="hidden md:flex flex-col justify-center items-center border border-theme-secondary-400 rounded-md mr-10">
-                        <div class="py-2 px-2">
-                            {!! $this->twoFactorQrCodeSvg !!}
-                        </div>
-                        <div class="border-t border-theme-secondary-400 w-full text-center mt-1 py-2 bg-theme-secondary-100 rounded-b-md">
-                            <span class="text-theme-secondary-900">{{ $this->state['two_factor_secret'] }}</span>
-                        </div>
+            <div class="flex flex-col sm:hidden">
+                <span class="text-lg font-bold leading-7 text-theme-secondary-900">
+                    @lang('fortify::pages.user-settings.2fa_not_enabled_title')
+                </span>
+                <div class="mt-2 text-base leading-7 text-theme-secondary-600">
+                    @lang('fortify::pages.user-settings.2fa_summary')
+                </div>
+            </div>
+
+            <div class="flex sm:hidden w-full mt-8 space-y-4">
+                <div class="w-full">
+                    <x-ark-input
+                        type="number"
+                        name="state.otp"
+                        :label="trans('fortify::pages.user-settings.one_time_password')"
+                        :errors="$errors"
+                    />
+                </div>
+            </div>
+
+            <hr class="flex sm:hidden my-8 border-t border-theme-primary-100">
+
+            <div class="flex flex-col sm:flex-row items-center sm:items-start sm:mt-8">
+                <div class="flex flex-col justify-center items-center border border-theme-secondary-400 rounded-md sm:mr-10">
+                    <div class="py-2 px-2">
+                        {!! $this->twoFactorQrCodeSvg !!}
+                    </div>
+                    <div class="border-t border-theme-secondary-400 w-full text-center mt-1 py-2 bg-theme-secondary-100 rounded-b-md">
+                        <span class="text-theme-secondary-900">{{ $this->state['two_factor_secret'] }}</span>
+                    </div>
+                </div>
+
+                <div class="flex h-64 w-1 bg-theme-primary-100 mr-10"></div>
+
+                <div class="hidden sm:flex flex-col">
+                    <span class="text-lg font-bold leading-7 text-theme-secondary-900">
+                        @lang('fortify::pages.user-settings.2fa_not_enabled_title')
+                    </span>
+                    <div class="mt-2 text-base leading-7 text-theme-secondary-600">
+                        @lang('fortify::pages.user-settings.2fa_summary')
                     </div>
 
-                    <div class="flex flex-col justify-between md:pl-10">
-                        <div class="flex flex-col">
-                            <span class="text-lg font-bold leading-7 text-theme-secondary-900">@lang('fortify::pages.user-settings.2fa_not_enabled_title')</span>
-
-                            <div class="mt-2 text-base leading-7 text-theme-secondary-600">
-                                @lang('fortify::pages.user-settings.2fa_summary')
-                            </div>
-                        </div>
-                        <div class="flex flex-col items-start mt-8 md:hidden">
-                            <div class="flex flex-col justify-center items-center border border-theme-secondary-400 rounded-md mr-10">
-                                <div class="py-2 px-2">
-                                    {!! $this->twoFactorQrCodeSvg !!}
-                                </div>
-                                <div class="border-t border-theme-secondary-400 w-full text-center mt-1 py-2 bg-theme-secondary-100 rounded-b-md">
-                                    <span class="text-theme-secondary-900">{{ $this->state['two_factor_secret'] }}</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="mt-8 space-y-4">
-                            <x-ark-input type="number" name="state.otp" :label="trans('fortify::pages.user-settings.one_time_password')" :errors="$errors" />
+                    <div class="hidden md:flex w-full mt-8">
+                        <div class="w-full">
+                            <x-ark-input
+                                type="number"
+                                name="state.otp"
+                                :label="trans('fortify::pages.user-settings.one_time_password')"
+                                :errors="$errors"
+                            />
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="flex justify-end mt-8">
-                <button type="button" class="button-secondary" wire:click="enableTwoFactorAuthentication">@lang('fortify::actions.enable')</button>
+
+            <div class="hidden sm:flex md:hidden w-full mt-8 space-y-4">
+                <div class="w-full">
+                    <x-ark-input
+                        type="number"
+                        name="state.otp"
+                        :label="trans('fortify::pages.user-settings.one_time_password')"
+                        :errors="$errors"
+                    />
+                </div>
+            </div>
+
+            <div class="flex sm:justify-end mt-8">
+                <button
+                    type="button"
+                    class="button-secondary w-full sm:w-auto"
+                    wire:click="enableTwoFactorAuthentication"
+                >
+                    @lang('fortify::actions.enable')
+                </button>
             </div>
         </div>
     @else
@@ -50,21 +93,25 @@
 
             <div class="flex mt-8">
                 <div class="flex flex-col">
-                    <strong>@lang('fortify::pages.user-settings.2fa_enabled_title')</strong>
-                    <div class="mt-2 text-sm text-theme-secondary-600">
+                    <span class="text-lg font-bold leading-7 text-theme-secondary-900">
+                        @lang('fortify::pages.user-settings.2fa_enabled_title')
+                    </span>
+                    <div class="mt-2 text-theme-secondary-600">
                         @lang('fortify::pages.user-settings.2fa_summary')
                     </div>
                 </div>
             </div>
 
-            <div class="flex justify-end mt-8 space-x-3">
-                <button type="submit" class="button-primary" wire:click="disableTwoFactorAuthentication">@lang('fortify::actions.disable')</button>
+            <div class="flex sm:justify-end mt-8 space-x-3 w-full">
+                <button type="submit" class="button-primary w-full sm:w-auto" wire:click="disableTwoFactorAuthentication">
+                    @lang('fortify::actions.disable')
+                </button>
             </div>
         </div>
     @endif
 
     @if($this->modalShown)
-        <x-ark-modal title-class="header-2">
+        <x-ark-modal class="mb-20" title-class="header-2">
             @slot('title')
                 @lang('fortify::pages.user-settings.2fa_reset_code_title')
             @endslot

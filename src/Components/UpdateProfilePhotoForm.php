23b8ab60d39b4092123889735c755318c35414e9
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-02-22 12:44:00 -0600

    Make it possible to change the form classes

--- src/Components/UpdateProfilePhotoForm.php
+++ src/Components/UpdateProfilePhotoForm.php
@@ -16,11 +16,14 @@ class UpdateProfilePhotoForm extends Component
     public string $dimensions = '';
 
     public string $alignment = '';
+    
+    public string $formClass = '';
 
-    public function mount(string $dimensions = 'w-48 h-48', string $alignment = 'items-center mb-4 md:items-start')
+    public function mount(string $dimensions = 'w-48 h-48', string $alignment = 'items-center mb-4 md:items-start', string $formClass = '')
     {
         $this->dimensions = $dimensions;
         $this->alignment  = $alignment;
+        $this->formClass  = $formClass;
     }
 
     public function render(): \Illuminate\View\View

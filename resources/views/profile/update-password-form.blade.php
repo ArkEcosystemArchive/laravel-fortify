896ec93f7cb915567dbdc068dcf51460d6e615ab
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-03-16 21:09:49 +0000

    refactor: tidy up some components

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -3,21 +3,60 @@
         <div>
             <x-ark-flash />
         </div>
-        <span class="header-4">@lang('fortify::pages.user-settings.password_information_title')</span>
-        <span>@lang('fortify::forms.update-password.requirements_notice')</span>
+
+        <span class="header-4">
+            @lang('fortify::pages.user-settings.password_information_title')
+        </span>
+
+        <span>
+            @lang('fortify::forms.update-password.requirements_notice')
+        </span>
     </div>
+
     <form class="mt-8" wire:submit.prevent="updatePassword">
         <div class="space-y-4">
-            <x-ark-input type="password" name="current_password" model="state.current_password" :label="trans('fortify::forms.current_password')" :errors="$errors" />
+            <x-ark-input
+                type="password"
+                name="current_password"
+                model="state.current_password"
+                :label="trans('fortify::forms.current_password')"
+                :errors="$errors"
+            />
 
-            <x:ark-fortify::password-rules class="w-full" :password-rules="$passwordRules" is-typing="isTyping" rules-wrapper-class="grid gap-4 my-4 sm:grid-cols-2 lg:grid-cols-3">
-                <x-ark-input type="password" name="password" model="state.password" class="w-full" :label="trans('fortify::forms.new_password')" @keydown="isTyping=true" :errors="$errors" />
+            <x:ark-fortify::password-rules
+                class="w-full"
+                :password-rules="$passwordRules"
+                is-typing="isTyping"
+                rules-wrapper-class="grid gap-4 my-4 sm:grid-cols-2 lg:grid-cols-3"
+            >
+                <x-ark-input
+                    type="password"
+                    name="password"
+                    model="state.password"
+                    class="w-full"
+                    :label="trans('fortify::forms.new_password')"
+                    @keydown="isTyping=true"
+                    :errors="$errors"
+                />
             </x:ark-fortify::password-rules>
 
-            <x-ark-input type="password" name="password_confirmation" model="state.password_confirmation" :label="trans('fortify::forms.confirm_password')" :errors="$errors" />
+            <x-ark-input
+                type="password"
+                name="password_confirmation"
+                model="state.password_confirmation"
+                :label="trans('fortify::forms.confirm_password')"
+                :errors="$errors"
+            />
         </div>
+
         <div class="flex w-full mt-8 sm:justify-end">
-            <button dusk="update-password-form-submit" type="submit" class="w-full button-secondary sm:w-auto">@lang('fortify::actions.update')</button>
+            <button
+                dusk="update-password-form-submit"
+                type="submit"
+                class="w-full button-secondary sm:w-auto"
+            >
+                @lang('fortify::actions.update')
+            </button>
         </div>
     </form>
 </div>

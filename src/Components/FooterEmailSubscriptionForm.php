1b602127cc8694787c2f66506daadcd68049cd9b
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-03-23 13:31:54 +0000

    component

---
+++ src/Components/FooterEmailSubscriptionForm.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ARKEcosystem\Fortify\Components;
+
+use ARKEcosystem\Fortify\Actions\SubscribeToNewsletter;
+use Livewire\Component;
+
+final class FooterEmailSubscriptionForm extends Component
+{
+    public bool $subscribed = false;
+
+    public ?string $email = null;
+
+    public ?string $status = null;
+
+    public function subscribe()
+    {
+        $this->subscribe = false;
+        $this->status    = null;
+
+        $this->subscribed = SubscribeToNewsletter::execute($this->email, config('newsletter.defaultListName'));
+
+        if ($this->subscribed) {
+            $this->status = trans('fortify::messages.subscription.pending');
+        } else {
+            $this->status = trans('fortify::messages.subscription.duplicate');
+        }
+
+        $this->email = null;
+    }
+}

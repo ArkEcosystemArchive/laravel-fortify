31d7f6d41f6b7baa445e03987f9e8144c9bcd878
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2021-03-23 13:26:29 +0000

    refactor: update verify page design (#97)

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -39,6 +39,6 @@ return [
     'verify' => [
         'page_header'         => 'Verify Your Email Address',
         'link_description'    => 'A verification link has been sent to your email address.',
-        'resend_verification' => 'Before proceeding, please check your email for a verification link. If you did not receive the email, <button type="submit" class="link">click here to request another</button>.',
+        'resend_verification' => '<span>Before proceeding, please check your email for a verification link.</span> <span>If you did not receive the email, <button type="submit" class="link">click here to request another</button>.</span>',
     ],
 ];

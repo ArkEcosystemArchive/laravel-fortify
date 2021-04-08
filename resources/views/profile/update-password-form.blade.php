f4a8e8a9fd2cae51e2d89d52195bf10e1c4c4a60
Jolan <mc-somango@live.fr>
2021-04-08 12:39:23 +0200

    refactor: hide password requirements on password update (#120)

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -1,4 +1,4 @@
-<div dusk="update-password-form">
+<div x-data="{isTyping: false}" dusk="update-password-form" @updated-password.window="isTyping = false">
     <div class="flex flex-col space-y-4 mt-4">
         <div>
             <x-ark-flash />
@@ -21,16 +21,25 @@
                 model="state.current_password"
                 :label="trans('fortify::forms.current_password')"
                 :errors="$errors"
+                class="w-full"
             />
 
-            <x-ark-input
-                type="password"
-                name="password"
-                model="state.password"
+            <x:ark-fortify::password-rules
                 class="w-full"
-                :label="trans('fortify::forms.new_password')"
-                :errors="$errors"
-            />
+                :password-rules="$passwordRules"
+                is-typing="isTyping"
+                rules-wrapper-class="grid gap-4 my-4 sm:grid-cols-2 lg:grid-cols-3"
+            >
+                <x-ark-input
+                    type="password"
+                    name="password"
+                    model="state.password"
+                    class="w-full"
+                    :label="trans('fortify::forms.new_password')"
+                    :errors="$errors"
+                    @keydown="isTyping=true"
+                />
+            </x:ark-fortify::password-rules>
 
             <x-ark-input
                 type="password"

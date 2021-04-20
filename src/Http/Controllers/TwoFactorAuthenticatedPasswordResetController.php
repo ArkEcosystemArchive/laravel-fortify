4002fc4770c7a79edf393f87bfa69ace7eec8728
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-04-20 17:30:10 -0500

    Cleanup

--- src/Http/Controllers/TwoFactorAuthenticatedPasswordResetController.php
+++ src/Http/Controllers/TwoFactorAuthenticatedPasswordResetController.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace ARKEcosystem\Fortify\Http\Controllers;
 
 use ARKEcosystem\Fortify\Http\Requests\TwoFactorResetPasswordRequest;
-use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Routing\Controller;
 use Illuminate\View\View;
@@ -14,30 +13,12 @@ use Laravel\Fortify\Contracts\FailedTwoFactorLoginResponse;
 class TwoFactorAuthenticatedPasswordResetController extends Controller
 {
     /**
-     * The guard implementation.
-     *
-     * @var \Illuminate\Contracts\Auth\StatefulGuard
-     */
-    protected $guard;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @param \Illuminate\Contracts\Auth\StatefulGuard $guard
-     *
-     * @return void
-     */
-    public function __construct(StatefulGuard $guard)
-    {
-        $this->guard = $guard;
-    }
-
-    /**
      * Show the two factor authentication challenge view.
      *
-     * @param \Laravel\Fortify\Http\Requests\TwoFactorLoginRequest $request
+     * @param TwoFactorResetPasswordRequest $request
+     * @param string $oken
      *
-     * @return View
+     * @return mixed
      */
     public function create(TwoFactorResetPasswordRequest $request, string $token): View
     {
@@ -57,13 +38,13 @@ class TwoFactorAuthenticatedPasswordResetController extends Controller
     }
 
     /**
-     * Attempt to authenticate a new session using the two factor authentication code.
+     * Validates the 2fa code and shows the reset password form
      *
      * @param TwoFactorResetPasswordRequest $request
      *
      * @return mixed
      */
-    public function store(TwoFactorResetPasswordRequest $request, string $token)
+    public function store(TwoFactorResetPasswordRequest $request): View
     {
         $user = $request->challengedUser();
 

b5d2de6aaa82dde22c1ddbc9b943269f0734543f
Alfonso Bribiesca <alfonsobries@gmail.com>
2021-04-23 04:17:20 -0500

    fix: auth form proper margins (#136)

--- resources/views/auth/register-form.blade.php
+++ resources/views/auth/register-form.blade.php
@@ -1,112 +1,113 @@
 <form
     method="POST"
     action="{{ $formUrl }}"
-    class="flex flex-col py-8 px-4 sm:px-8 mx-4 border-2 rounded-xl border-theme-secondary-200"
+    class="flex flex-col px-4 py-8 mx-4 border-2 sm:px-8 rounded-xl border-theme-secondary-200"
     x-data="{isTyping: false}"
 >
     @csrf
 
-    @if(Config::get('fortify.username_alt'))
-        <div class="mb-8">
-            <div class="flex flex-1">
-                <x-ark-input
-                    wire:model.defer="state.username"
-                    no-model
-                    type="text"
-                    name="username"
-                    :label="trans('fortify::forms.username')"
-                    autocomplete="username"
-                    class="w-full"
-                    :errors="$errors"
-                />
-            </div>
-        </div>
-    @endif
-
     @if($invitation)
         <input type="hidden" name="name" value="{{ $invitation->name }}" />
-    @else
-        <div class="mb-8">
-            <div class="flex flex-1">
-                <x-ark-input
-                    wire:model.defer="state.name"
-                    no-model
-                    name="name"
-                    :label="trans('fortify::forms.display_name')"
-                    autocomplete="name"
-                    class="w-full"
-                    :autofocus="true"
-                    :errors="$errors"
-                />
-            </div>
-        </div>
-    @endif
-
-    @if($invitation)
         <input type="hidden" name="email" value="{{ $invitation->email }}" />
-    @else
-        <div class="mb-8">
-            <div class="flex flex-1">
-                <x-ark-input
-                    wire:model.defer="state.email"
-                    no-model
-                    type="email"
-                    name="email"
-                    :label="trans('fortify::forms.email')"
-                    autocomplete="email"
-                    class="w-full"
-                    :errors="$errors"
-                />
-            </div>
-        </div>
     @endif
 
-    <x:ark-fortify::password-rules class="mb-8" :password-rules="$passwordRules" is-typing="isTyping" rules-wrapper-class="grid grid-cols-1 gap-4 mt-4">
-        <x-ark-input
-            model="state.password"
-            type="password"
-            name="password"
-            :label="trans('fortify::forms.password')"
-            autocomplete="new-password"
-            class="w-full mb-2"
-            @keydown="isTyping=true"
-            :errors="$errors"
-        />
-    </x:ark-fortify::password-rules>
+    <div class="space-y-5">
+        @if(Config::get('fortify.username_alt'))
+            <div>
+                <div class="flex flex-1">
+                    <x-ark-input
+                        wire:model.defer="state.username"
+                        no-model
+                        type="text"
+                        name="username"
+                        :label="trans('fortify::forms.username')"
+                        autocomplete="username"
+                        class="w-full"
+                        :errors="$errors"
+                    />
+                </div>
+            </div>
+        @endif
 
-    <div class="mb-4">
-        <div class="flex flex-1">
+        @unless($invitation)
+            <div>
+                <div class="flex flex-1">
+                    <x-ark-input
+                        wire:model.defer="state.name"
+                        no-model
+                        name="name"
+                        :label="trans('fortify::forms.display_name')"
+                        autocomplete="name"
+                        class="w-full"
+                        :autofocus="true"
+                        :errors="$errors"
+                    />
+                </div>
+            </div>
+
+            <div>
+                <div class="flex flex-1">
+                    <x-ark-input
+                        wire:model.defer="state.email"
+                        no-model
+                        type="email"
+                        name="email"
+                        :label="trans('fortify::forms.email')"
+                        autocomplete="email"
+                        class="w-full"
+                        :errors="$errors"
+                    />
+                </div>
+            </div>
+        @endunless
+
+        <x:ark-fortify::password-rules :password-rules="$passwordRules" is-typing="isTyping" rules-wrapper-class="grid grid-cols-1 gap-4 my-4">
             <x-ark-input
-                model="state.password_confirmation"
+                model="state.password"
                 type="password"
-                name="password_confirmation"
-                :label="trans('fortify::forms.confirm_password')"
+                name="password"
+                :label="trans('fortify::forms.password')"
                 autocomplete="new-password"
                 class="w-full"
+                @keydown="isTyping=true"
                 :errors="$errors"
             />
+        </x:ark-fortify::password-rules>
+
+        <div>
+            <div class="flex flex-1">
+                <x-ark-input
+                    model="state.password_confirmation"
+                    type="password"
+                    name="password_confirmation"
+                    :label="trans('fortify::forms.confirm_password')"
+                    autocomplete="new-password"
+                    class="w-full"
+                    :errors="$errors"
+                />
+            </div>
         </div>
-    </div>
 
-    <div class="mb-8">
-        <x-ark-checkbox
-            model="state.terms"
-            name="terms"
-            :errors="$errors"
-        >
-            @slot('label')
-                @lang('fortify::auth.register-form.conditions', ['termsOfServiceRoute' => route('terms-of-service'), 'privacyPolicyRoute' => route('privacy-policy')])
-            @endslot
-        </x-ark-checkbox>
+        <div>
+            <x-ark-checkbox
+                model="state.terms"
+                name="terms"
+                :errors="$errors"
+            >
+                @slot('label')
+                    @lang('fortify::auth.register-form.conditions', ['termsOfServiceRoute' => route('terms-of-service'), 'privacyPolicyRoute' => route('privacy-policy')])
+                @endslot
+            </x-ark-checkbox>
 
-        @error('terms')
-            <p class="input-help--error">{{ $message }}</p>
-        @enderror
-    </div>
+            @error('terms')
+                <p class="input-help--error">{{ $message }}</p>
+            @enderror
+        </div>
 
-    <div class="text-right">
-        <button type="submit" class="w-full button-secondary sm:w-auto">
-            @lang('fortify::actions.sign_up')
-        </button>
+        <div class="text-right">
+            <button type="submit" class="w-full button-secondary sm:w-auto">
+                @lang('fortify::actions.sign_up')
+            </button>
+        </div>
     </div>
 </form>

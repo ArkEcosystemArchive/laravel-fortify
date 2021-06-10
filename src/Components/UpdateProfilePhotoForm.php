00a0e47b3620d85302ed3dfaec5e86cbc9c32443
Maurizio <maurizio.lepora@gmail.com>
2021-06-10 10:15:42 +0200

    fix: add `WithResponsiveImages`

--- src/Components/UpdateProfilePhotoForm.php
+++ src/Components/UpdateProfilePhotoForm.php
@@ -50,6 +50,7 @@ class UpdateProfilePhotoForm extends Component
 
         $this->user
             ->addMedia($this->imageSingle->getRealPath())
+            ->withResponsiveImages()
             ->usingName($this->imageSingle->hashName())
             ->toMediaCollection('photo');
 

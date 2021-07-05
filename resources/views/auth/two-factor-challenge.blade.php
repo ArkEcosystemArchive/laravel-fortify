6feffaf1c8be4b940daf542e8c05e86b7435f0f1
Highjhacker <jolanbeer@gmail.com>
2021-07-05 11:55:32 +0200

    refactor: requested changes

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -46,7 +46,7 @@
                         type="text"
                         name="code"
                         :label="trans('fortify::forms.2fa_code')"
-                        class="w-full"
+                        class="w-full disabled-arrows-input"
                         :errors="$errors"
                         autocomplete="one-time-code"
                         input-mode="numeric"

cd5e655f0287ac801b658755f80e5d74b1c97c11
Jolan <mc-somango@live.fr>
2021-07-13 12:11:13 +0200

    refactor: use autocomplete attribute on various input elements (#159)

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -232,7 +232,14 @@
                 </div>
                 <form class="mt-8">
                     <div class="space-y-2">
-                        <x-ark-input type="password" name="password" model="confirmedPassword" :label="trans('fortify::forms.password')" />
+                        <input type="hidden" autocomplete="email" />
+                        <x-ark-input
+                            type="password"
+                            name="password"
+                            model="confirmedPassword"
+                            :label="trans('fortify::forms.password')"
+                            autocomplete="current-password"
+                        />
                     </div>
                 </form>
             </x-slot>

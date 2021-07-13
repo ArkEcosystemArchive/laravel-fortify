3579b9aee38cc249eae95a03d95882158fbb3f7d
Jolan <mc-somango@live.fr>
2021-07-13 11:55:22 +0200

    refactor: add dusk selector to 2fa form (#158)

--- resources/views/profile/two-factor-authentication-form.blade.php
+++ resources/views/profile/two-factor-authentication-form.blade.php
@@ -14,7 +14,7 @@
     ])->render();
 @endphp
 
-<div>
+<div dusk="two-factor-authentication-form">
     @if (! $this->enabled)
         <div class="flex flex-col w-full space-y-8">
             <div class="flex flex-col">
@@ -87,6 +87,7 @@
                                 :errors="$errors"
                                 pattern="[0-9]{6}"
                                 class="hide-number-input-arrows"
+                                dusk="one-time-password"
                             />
                         </div>
                     </div>
@@ -111,6 +112,7 @@
                     type="button"
                     class="w-full button-secondary sm:w-auto"
                     wire:click="enableTwoFactorAuthentication"
+                    dusk="enable-two-factor-authentication"
                 >
                     @lang('fortify::actions.enable')
                 </button>
@@ -142,69 +144,76 @@
                     @lang('fortify::actions.recovery_codes')
                 </button>
 
-                <button type="submit" class="w-full button-primary sm:w-auto" wire:click="disableTwoFactorAuthentication">
+                <button
+                    type="submit"
+                    class="w-full button-primary sm:w-auto"
+                    wire:click="disableTwoFactorAuthentication"
+                    dusk="disable-two-factor-authentication"
+                >
                     @lang('fortify::actions.disable')
                 </button>
             </div>
         </div>
     @endif
 
-    @if($this->modalShown)
-        <x-ark-modal title-class="header-2">
-            @slot('title')
-                @lang('fortify::pages.user-settings.2fa_reset_code_title')
-            @endslot
+    <div dusk="recovery-codes-modal">
+        @if($this->modalShown)
+            <x-ark-modal title-class="header-2">
+                @slot('title')
+                    @lang('fortify::pages.user-settings.2fa_reset_code_title')
+                @endslot
 
-            @slot('description')
-                <div class="flex flex-col mt-8 space-y-4">
-                    <x-ark-alert type="warning">
-                        <x-slot name="message">
-                            @lang('fortify::pages.user-settings.2fa_warning_text')
-                        </x-slot>
-                    </x-ark-alert>
-                    <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-4">
-                        @foreach (json_decode(decrypt($this->user->two_factor_recovery_codes), true) as $code)
-                            <div class="flex items-center h-12 font-medium border rounded border-theme-secondary-300 text-theme-secondary-900">
-                                <span class="flex items-center justify-center w-8 h-full rounded-l bg-theme-secondary-100">
-                                    {{ $loop->index + 1 }}
-                                </span>
-                                <input
-                                    type="text"
-                                    id="resetCode_{{ $loop->index }}"
-                                    class="w-full ml-4"
-                                    value="{{ $code }}"
-                                    readonly
-                                />
-                            </div>
-                        @endforeach
-                        {{-- TODO: check if we need this or not --}}
-                        {{-- <div class="mt-6">
-                            <x-ark-clipboard :value="$this->resetCode"/>
-                        </div> --}}
+                @slot('description')
+                    <div class="flex flex-col mt-8 space-y-4">
+                        <x-ark-alert type="warning">
+                            <x-slot name="message">
+                                @lang('fortify::pages.user-settings.2fa_warning_text')
+                            </x-slot>
+                        </x-ark-alert>
+                        <div class="grid grid-flow-row grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-4">
+                            @foreach (json_decode(decrypt($this->user->two_factor_recovery_codes), true) as $code)
+                                <div class="flex items-center h-12 font-medium border rounded border-theme-secondary-300 text-theme-secondary-900">
+                                    <span class="flex items-center justify-center w-8 h-full rounded-l bg-theme-secondary-100">
+                                        {{ $loop->index + 1 }}
+                                    </span>
+                                    <input
+                                        type="text"
+                                        id="resetCode_{{ $loop->index }}"
+                                        class="w-full ml-4"
+                                        value="{{ $code }}"
+                                        readonly
+                                    />
+                                </div>
+                            @endforeach
+                            {{-- TODO: check if we need this or not --}}
+                            {{-- <div class="mt-6">
+                                <x-ark-clipboard :value="$this->resetCode"/>
+                            </div> --}}
+                        </div>
                     </div>
-                </div>
-            @endslot
+                @endslot
 
-            @slot('buttons')
-                <div class="flex flex-col-reverse w-full sm:flex-row sm:justify-between">
-                    <div class="flex justify-center w-full mt-3 sm:justify-start sm:mt-0">
-                        <x-ark-file-download
-                            :filename="'2fa_recovery_code_' . $this->user->name"
-                            :content="implode('\n', json_decode(decrypt($this->user->two_factor_recovery_codes)))"
-                            :title="trans('fortify::actions.download')"
-                            wrapper-class="w-full sm:w-auto"
-                            class="justify-center w-full"
-                        />
+                @slot('buttons')
+                    <div class="flex flex-col-reverse w-full sm:flex-row sm:justify-between">
+                        <div class="flex justify-center w-full mt-3 sm:justify-start sm:mt-0">
+                            <x-ark-file-download
+                                :filename="'2fa_recovery_code_' . $this->user->name"
+                                :content="implode('\n', json_decode(decrypt($this->user->two_factor_recovery_codes)))"
+                                :title="trans('fortify::actions.download')"
+                                wrapper-class="w-full sm:w-auto"
+                                class="justify-center w-full"
+                            />
+                        </div>
+                        <div class="flex justify-center">
+                            <button class="items-center w-full button-primary sm:w-auto whitespace-nowrap" wire:click="hideRecoveryCodes" dusk="recovery-codes-understand">
+                                @lang('fortify::actions.understand')
+                            </button>
+                        </div>
                     </div>
-                    <div class="flex justify-center">
-                        <button class="items-center w-full button-primary sm:w-auto whitespace-nowrap" wire:click="hideRecoveryCodes">
-                            @lang('fortify::actions.understand')
-                        </button>
-                    </div>
-                </div>
-            @endslot
-        </x-ark-modal>
-    @endif
+                @endslot
+            </x-ark-modal>
+        @endif
+    </div>
 
     @if($this->confirmPasswordShown)
         <x-ark-modal title-class="header-2" width-class="max-w-2xl" wire-close="closeConfirmPassword">

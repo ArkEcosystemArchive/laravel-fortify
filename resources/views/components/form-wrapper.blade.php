cb25bf5e213076a4e895f4b1067a19ef6bdaf354
Maurizio <maurizio.lepora@gmail.com>
2021-07-21 16:50:38 +0200

    chore: remove redundant padding from form-wrapper.blade.php

--- resources/views/components/form-wrapper.blade.php
+++ resources/views/components/form-wrapper.blade.php
@@ -1,7 +1,7 @@
 @props(['action', 'method' => 'POST'])
 
 <div class="sm:max-w-xl py-8 mx-auto" {{ $attributes }}>
-    <form action="{{ $action }}" method="{{ $method }}" class="flex flex-col px-8 pb-8 sm:pt-8 border-b-2 sm:border-2 sm:rounded-xl sm:px-8 border-theme-secondary-200">
+    <form action="{{ $action }}" method="{{ $method }}" class="flex flex-col px-8 pb-8 sm:pt-8 border-b-2 sm:border-2 sm:rounded-xl border-theme-secondary-200">
         @csrf
         {{ $slot  }}
     </form>

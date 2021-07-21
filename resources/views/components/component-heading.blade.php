7cb8d53bfc0d8762a1eb9586c0dd972094beab44
Maurizio <maurizio.lepora@gmail.com>
2021-07-21 11:45:53 +0200

    chore: change component heading font-size and line-height

--- resources/views/components/component-heading.blade.php
+++ resources/views/components/component-heading.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full py-8 bg-theme-secondary-100">
   <div class="container mx-auto ">
-    <h1 class="mx-4 text-2xl font-bold text-center md:text-4xl md:mx-8 xl:mx-16 text-theme-secondary-900">{{ $title }}</h1>
-    <p class="mx-4 mt-4 font-semibold text-center text-theme-secondary-700 md:mx-8 xl:mx-16">{{ $description }}</p>
+    <h1 class="mx-4 text-3xl font-bold text-center md:text-4xl md:mx-8 xl:mx-16 text-theme-secondary-900">{{ $title }}</h1>
+    <p class="mx-8 mt-4 text-lg leading-8 font-semibold text-center text-theme-secondary-700 md:mx-8 xl:mx-16">{{ $description }}</p>
   </div>
 </div>

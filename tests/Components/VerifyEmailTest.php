f2a7f28d7b30da61ac96daff23fa8f472cec6204
Brian Faust <hello@basecode.sh>
2021-09-27 06:45:34 +0300

    wip

--- tests/Components/VerifyEmailTest.php
+++ tests/Components/VerifyEmailTest.php
@@ -10,7 +10,8 @@ it('can resend a verification email', function (): void {
     Livewire::actingAs(createUserModel())
         ->test(VerifyEmail::class)
         ->call('resend')
-        ->assertDontSee(trans('fortify::messages.resend_email_verification_limit'));
+        ->assertDontSeeHtml('wire:loading.attr="disabled"')
+        ->assertSeeHtml(sprintf('data-tippy-content="%s"', trans('fortify::messages.resend_email_verification_limit')));
 });
 
 it('can resend a verification email once every 5 minutes', function (): void {
@@ -18,9 +19,13 @@ it('can resend a verification email once every 5 minutes', function (): void {
         ->test(VerifyEmail::class)
         ->call('resend')
         ->call('resend')
-        ->assertSee(trans('fortify::messages.resend_email_verification_limit'));
+        ->assertDontSeeHtml('wire:loading.attr="disabled"')
+        ->assertSeeHtml(sprintf('data-tippy-content="%s"', trans('fortify::messages.resend_email_verification_limit')));
 
     $this->travel(6)->minutes();
 
-    $component->call('$refresh')->assertDontSee(trans('fortify::messages.resend_email_verification_limit'));
+    $component
+        ->call('$refresh')
+        ->assertSeeHtml('wire:loading.attr="disabled"')
+        ->assertDontSeeHtml(sprintf('data-tippy-content="%s"', trans('fortify::messages.resend_email_verification_limit')));
 });
